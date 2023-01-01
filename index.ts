8286cedaeb22447167d0c14a965da488f91a32e9
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-01 17:42:56 +0530

    cleanup

--- index.ts
+++ index.ts
@@ -84,32 +84,20 @@ app.use(function (req, res, next) {
   next();
 });
 
-const authCheck = (req, res, next) => {
-  if (!req.user) {
-    res.status(401).json({
-      authenticated: false,
-      message: 'user has not been authenticated',
-    });
-  } else {
-    next();
-  }
-};
-
-// if it's already login, send the profile response,
-// otherwise, send a 401 response that the user is not authenticated
-// authCheck before navigating to home page
-// app.get('/', authCheck, (req, res) => {
-//   res.status(200).json({
-//     authenticated: true,
-//     message: 'user successfully authenticated',
-//     user: req.user,
-//     cookies: req.cookies,
-//   });
-// });
+// const authCheck = (req, res, next) => {
+//   if (!req.user) {
+//     res.status(401).json({
+//       authenticated: false,
+//       message: 'user has not been authenticated',
+//     });
+//   } else {
+//     next();
+//   }
+// };
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
-app.use('/users', authCheck, usersRouter);
+app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 
 app.listen(PORT, () => {
