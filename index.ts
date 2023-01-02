b71b2a11931d842bd566ae6fe956580f8622d4a0
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-02 14:38:24 +0530

    Revert "express session"
    This reverts commit ee3cdf02d70a4030520e72dc644377cd4542c301.

--- index.ts
+++ index.ts
@@ -5,9 +5,9 @@ const cookieSession = require('cookie-session');
 const helmet = require('helmet');
 const cors = require('cors');
 const passport = require('passport');
-
+import MongoDBStore from 'connect-mongodb-session';
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const mongoStore = MongoDBStore(session);
 
 const indexRouter = require('./routes/index.route');
 const postsRouter = require('./routes/posts.route');
@@ -18,7 +18,14 @@ dotenv.config();
 
 const passportSetup = require('./config/passport-setup');
 
+const store = new mongoStore({
+  collection: 'userSessions',
+  uri: process.env.DB_URL,
+  expires: 1000,
+});
+
 const PORT = process.env.PORT;
+
 const app: Express = express();
 
 // cors policy
@@ -43,31 +50,24 @@ const { mongooseConnection } = require('./connection/mongoose.connection');
 mongooseConnection();
 
 // cookie session
-// app.use(
-//   cookieSession({ name: 'session', keys: ['lama'], maxAge: 24 * 60 * 60 * 100 })
-// );
-
-// const store = new mongoStore({
-//   collection: 'userSessions',
-//   uri: process.env.DB_URL,
-//   expires: 1000,
-// });
+app.use(
+  cookieSession({ name: 'session', keys: ['lama'], maxAge: 24 * 60 * 60 * 100 })
+);
 
 // express session
-app.use(
-  session({
-    proxy: true,
-    secret: 'keyboard cat',
-    // resave: false, // don't save session if unmodified
-    // saveUninitialized: false, // don't create session until something stored
-    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
-    cookie: {
-      sameSite: 'none',
-      secure: false,
-      maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
-    },
-  })
-);
+// app.use(
+//   session({
+//     secret: 'keyboard cat',
+//     resave: false, // don't save session if unmodified
+//     saveUninitialized: false, // don't create session until something stored
+//     store: store,
+//     cookie: {
+//       sameSite: 'none',
+//       secure: false,
+//       maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
+//     },
+//   })
+// );
 
 // initialize passport
 // app.use(passport.initialize());
