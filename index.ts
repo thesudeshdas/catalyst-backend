54406694c76cf5bdb3b95d771e7ac311c356c469
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-02 14:04:49 +0530

    check proxy

--- index.ts
+++ index.ts
@@ -47,6 +47,7 @@ mongooseConnection();
 // express session
 app.use(
   session({
+    proxy: true,
     secret: 'keyboard cat',
     // resave: false, // don't save session if unmodified
     // saveUninitialized: false, // don't create session until something stored
