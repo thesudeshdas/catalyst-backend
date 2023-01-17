415c9ef82462c6b326a31fee46d0f016d5b116e1
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-17 19:48:17 +0530

    feat: jwt auth

--- routes/users.route.ts
+++ routes/users.route.ts
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const user_controller = require('../controllers/user.controller');
 
+const middlewares = require('../middlewares/auth.middleware');
+
 router.get('/', user_controller.user_list_get);
 
 router.post('/sign-up', user_controller.user_sign_up);
@@ -13,10 +15,22 @@ router.param('userId', user_controller.user_find_param);
 
 router.get('/:userId', user_controller.user_details_get);
 
-router.post('/:userId', user_controller.user_update_details_post);
+router.post(
+  '/:userId',
+  middlewares.auth_verification,
+  user_controller.user_update_details_post
+);
 
-router.post('/:userId/follow', user_controller.user_follow_post);
+router.post(
+  '/:userId/follow',
+  middlewares.auth_verification,
+  user_controller.user_follow_post
+);
 
-router.post('/:userId/unfollow', user_controller.user_unfollow_post);
+router.post(
+  '/:userId/unfollow',
+  middlewares.auth_verification,
+  user_controller.user_unfollow_post
+);
 
 module.exports = router;
